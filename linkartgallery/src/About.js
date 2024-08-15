af480b972889760d7e68e281369f3865b7b21344
Wangui <elswangui@gmail.com>
2024-08-15 19:37:57 +0300

    createAbout.js and add information

---
+++ linkartgallery/src/About.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+
+function About(){
+    return(
+        <div className="about-us">
+            <h1>About Us</h1>
+            <p>
+            Welcome to LinkArt Studio, where creativity meets community. Established with the vision of bridging the gap between emerging artists and art enthusiasts, our gallery has become a dynamic space for artistic expression and cultural exchange. 
+            </p>
+            <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpjDV5K7GuRkpJfFmL_2L8c4Ac4XM7r7C7BA&s"} width="50%" height="50%"  alt="" />
+            <h2>Background</h2>
+            <p>Founded in 2020, LinkArt Studio began as a small collective of local and global artists passionate about sharing their work with the world. Over the years, we have grown into a recognized name in the art world, hosting numerous exhibitions, workshops, and events that celebrate the diverse voices of contemporary art. Our mission is to support and showcase the talents of both established and up-and-coming artists, providing them with a platform to reach new audiences.</p>
+            
+            
+            </div>
+            
+    )
+}
+export default About
