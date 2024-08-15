047ad38f72863b82268cadfed67cb77af1288a5d
Wangui <elswangui@gmail.com>
2024-08-15 21:18:05 +0300

    display a collection of artwork

--- linkartgallery/src/Gallery.js
+++ linkartgallery/src/Gallery.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React,{useState} from "react"
+import ArtworkDetails from "./ArtworkDetails"; 
 
 
-function Gallery({arts,addToMyCart}){
-    return(
-        <div className="art-collection">
-        {arts.map(art=>(
-            <div key={art.id} className="art-profile" >
-             <h2>{art.name}</h2>
-             <p>Title {art.title}</p>
-             <p>Artist {art.artist}</p>
-             <p>Year {art.year}</p>
-             <p>DEscription {art.description}</p>
-             
-             <img src={art.avatar_url} alt={`${art.name} profile`} />
-             <button onClick={() => addToMyCart(art)}>Add to My Cart</button>
-                </div>
-      )
+function Gallery({ arts, addToMyCart,addToMyWishList }) {
+  const [selectedArt, setSelectedArt] = useState(null);
 
-    )}
+  const handleSelectArt = (art) => {//function is called when an artwork is selected.
+    setSelectedArt(art);
+  };
+
+  const handleCloseDetails = () => {
+    setSelectedArt(null); // function is called when the user wants to close the details view deselecting the artwork.
+  };
+
+  return(
+  <div className={`gallery-container ${selectedArt ? 'blur-background' : ''}`}>
+  <div className="art-collection">
+    {arts.map((art) => (
+      <div key={art.id} className="art-profile" onClick={() => handleSelectArt(art)}>
+        <img src={art.imageUrl} width="200" alt={`${art.title} profile`} className="art-image" />
+        <p>{art.title}</p>
+        <p>{art.artist}</p>
+      </div>
+    ))}
+  </div>
+  {selectedArt && (
+        <div className="artwork-details-overlay">
+          <ArtworkDetails art={selectedArt} addToMyCart={addToMyCart} addToMyWishList={addToMyWishList}/>
+          <button className="close-details" onClick={handleCloseDetails}>x</button>
+        </div>
+      )}
+  
+ 
 </div>
-)
+);
 }
+
 export default Gallery
