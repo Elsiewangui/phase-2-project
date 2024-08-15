0ba60f2d4b0894f02e8bed9695e95564644c7938
Wangui <elswangui@gmail.com>
2024-08-15 20:52:49 +0300

    add an handlechange function to search

--- linkartgallery/src/Search.js
+++ linkartgallery/src/Search.js
@@ -1,37 +1,25 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-function Search({ arts, populateArtList, showError }) {
-    const [searchTerm, setSearchTerm] = useState("");
 
-    const handleSearchChange = (event) => {
-        const value = event.target.value.toLowerCase();
-        setSearchTerm(value);
+const Search = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
 
-        if (value === "") {
-            populateArtList(arts); // Show all art if search bar is empty
-        } else {
-            const filteredArts= books.filter(art =>
-                art.title.toLowerCase().includes(value) ||
-                art.artist.toLowerCase().includes(value)
-            );
-            if (filteredArts.length > 0) {
-                populateBookList(filteredArts);
-            } else {
-                showError("No title/artist found matching your search.");
-            }
-        }
-    };
+  const handleChange = (e) => {// function is called when typing in the search input field.
+    const value = e.target.value;
+    setSearchTerm(value); 
+    onSearch(value); // Passes the search term to the parent
+  };
 
-    return (
-        <div>
-            <input
-                type="text"
-                value={searchTerm}
-                onChange={handleSearchChange}
-                placeholder="Search by title or artist"
-            />
-        </div>
-    );
-}
+  return (
+    <div className='search'>
+      <input
+        type="text"
+        placeholder='Search by title or artist'
+        value={searchTerm}
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
 
 export default Search;
